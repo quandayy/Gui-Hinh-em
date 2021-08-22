5c56b1334a4c17760518223d5522555c1c762502
quandayy <89348584+quandayy@users.noreply.github.com>
2021-08-22 21:58:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'Xin chào Hinh em!',
+    introDesc: `Ngước mặt lên hỏi ông trời, tại sao lại có anh chàng đẹp trai như Quân???`,
+    btnIntro: 'Ngoan, bấm vào đây',
+    title: 'Anh cực kỳ đẹp trai đúng không?',
+    desc: 'Thoát ra làm chó ',
+    btnYes: 'Quân anh đẹp trai nhất',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    question:'Có phải em thấy anh rất đẹp đúng không?',
+    btnReply: 'Gửi cho anh',
+    reply: 'Dạ vâng đúng vậy',
+    mess: 'Anh biết mà, em quá khen anh ngại',
+    messDesc: 'Thế thôi nhé.',
+    btnAccept: 'Tạm biệt anh đẹp trai',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
